8733c4a7793961eca82cf30f591a4737310d4b3b
Azat Khuzhin <a3at.mail@gmail.com>
2013-02-04 23:13:53 +0400

    cmake modules to as git submodule
    Drop cmake as folder. And create small repository for cmake modules.
    Because I can use it in another project.

--- cmake/Modules/AddFilesGlobRecursiveToList.cmake
+++
@@ -1,9 +0,0 @@
-
-# TODO: add all options for file() and list(APPEND )
-
-macro(AddFilesGlobRecursiveToList _list_name _glob_pattern)
-    unset(AddFilesGlobRecursiveToList_tmp CACHE)
-
-    file(GLOB_RECURSE AddFilesGlobRecursiveToList_tmp ${_glob_pattern})
-    list(APPEND ${_list_name} ${AddFilesGlobRecursiveToList_tmp})
-endmacro(AddFilesGlobRecursiveToList)
