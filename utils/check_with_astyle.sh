c3ba1655fb0c963019cb1880d9dca430e69b09ea
Azat Khuzhin <a3at.mail@gmail.com>
2013-03-17 03:24:59 +0400

    [utils] add syntax checker using astyle.

---
+++ utils/check_with_astyle.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#
+# TODO: add more options
+#
+
+DIRNAME=$(which dirname)
+
+set -e
+
+if [ ! "$DIRNAME" ]; then
+    echo "dirname is not available" >&2
+    exit 1
+fi
+
+SELF=$($DIRNAME "$0")
+BIN=$(which astyle)
+CONFIG=$(cat "$SELF/boostcache_astyle")
+SRCDIR=$(readlink -f "$SELF/../src")
+
+set +e
+if ! $($BIN --version 2>&1 | fgrep -q "(with some patches from https://github.com/azat/astyle)"); then
+    echo "astyle version didn't support some features." >&2
+    echo "See at https://github.com/azat/astyle" >&2
+    exit 2
+fi
+set -e
+
+# TODO: enhanced file "iterator"
+for FILE in $(find $SRCDIR -type f | fgrep -v src/third_party/); do
+    $BIN --check-only $CONFIG "$FILE"
+done
