ebec79c2419636bc65762d4f3faa601c9a961e24
Azat Khuzhin <a3at.mail@gmail.com>
2013-04-03 01:44:53 +0400

    [server] drop nesting level in main() for "--fork" option

--- src/server/boostcached.cpp
+++ src/server/boostcached.cpp
@@ -20,11 +20,9 @@ int main(int argc, char **argv)
     Options options;
     options.parse(argc, argv);
 
-    if (options.getValue("fork")) {
-        // TODO: VERIFY macros
-        if (daemon(0, 0) == -1) {
-            return EXIT_FAILURE;
-        }
+    // TODO: VERIFY macros
+    if (options.getValue("fork") && (daemon(0, 0) == -1)) {
+        return EXIT_FAILURE;
     }
 
     CommandServer server(CommandServer::Options(options.getValue<int>("port")));
