bee33415f8e612ad56834584535383cdfb77836f
Azat Khuzhin <a3at.mail@gmail.com>
2013-04-03 01:24:47 +0400

    [server] add "--fork" option, that call daemon()

--- src/server/boostcached.cpp
+++ src/server/boostcached.cpp
@@ -11,12 +11,21 @@
 #include "server/options.h"
 #include "kernel/net/commandserver.h"
 
+#include <unistd.h>
+
 using namespace Server;
 
 int main(int argc, char **argv)
 {
     Options options;
     options.parse(argc, argv);
+
+    if (options.getValue("fork")) {
+        // TODO: VERIFY macros
+        if (daemon(0, 0) == -1) {
+            return EXIT_FAILURE;
+        }
+    }
 
     CommandServer server(CommandServer::Options(options.getValue<int>("port")));
     server.start();
--- src/server/options.cpp
+++ src/server/options.cpp
@@ -15,9 +15,13 @@ namespace Server
     void Options::additionalOptions()
     {
         visibleOptions.add_options()
-            ("port,p", boost::program_options::value<int>()->default_value(9876), "Port listen to")
-            ("host,H", boost::program_options::value<std::string>(), "Host listen to")
-            ("socket,s", boost::program_options::value<std::string>()->default_value("boostcached.sock"), "Socket listen to")
+            ("port,p", boost::program_options::value<int>()->default_value(9876),
+             "Port listen to")
+            ("host,H", boost::program_options::value<std::string>(),
+             "Host listen to")
+            ("socket,s", boost::program_options::value<std::string>()->default_value("boostcached.sock"),
+             "Socket listen to")
+            ("fork,f", "Fork server process")
         ;
     }
 }
