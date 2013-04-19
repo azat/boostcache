c0f1b4365d2ffa19cc6d3735d392d35b30e1c430
Azat Khuzhin <a3at.mail@gmail.com>
2013-04-19 17:45:40 +0400

    [benchmarks] rewrite nc part of one_connection to gawk.

--- benchmarks/one_connection.sh
+++ benchmarks/one_connection.sh
@@ -9,7 +9,7 @@ fi
 
 INPUT_FIFO="/tmp/test-boostcached-input.$RANDOM"
 OUTPUT_FIFO="/tmp/test-boostcached-output.$RANDOM"
-PORT=9876
+#PORT=9876
 HOST=localhost
 #LIMIT=10000000
 LIMIT=10
@@ -17,14 +17,30 @@ LIMIT=10
 mkfifo "$INPUT_FIFO"
 mkfifo "$OUTPUT_FIFO"
 
-cat "$INPUT_FIFO" | tee in.log | nc $HOST $PORT | tee out.log > $OUTPUT_FIFO &
+# Server client
+cat "$INPUT_FIFO" | tee in.log | \
+    gawk 'BEGIN {
+        serverConnection = "/inet4/tcp/0/localhost/9876"
+    }
+    {
+        # write to server
+        print $0 |& serverConnection
+        # read from server
+        serverConnection |& getline
+        print $0
+    }
+    END {
+        close(service)
+    }' \
+    | tee out.log > $OUTPUT_FIFO &
+
 PID="$!"
 trap 'echo Killing nc, and removing tmp pipe; kill $PID; rm -f $INPUT_FIFO $OUTPUT_FIFO' EXIT
 
 function write_read()
 {
     echo $* >> $INPUT_FIFO
-    timeout 10s head -n1 $OUTPUT_FIFO
+    timeout 10s head -n1 $OUTPUT_FIFO || "Can't read anything from $OUTPUT_FIFO"
 }
 
 echo "Testing HSET ..."
