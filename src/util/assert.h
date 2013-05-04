e42725f7790d32ed1e7da8858b408c03da4306a1
Azat Khuzhin <a3at.mail@gmail.com>
2013-05-05 02:36:04 +0400

    [util] add ASSERT()/VERIFY() macros

---
+++ src/util/assert.h
@@ -0,0 +1,19 @@
+
+/**
+ * This file is part of the boostcache package.
+ *
+ * (c) Azat Khuzhin <a3at.mail@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * @brief Assert macros (just aliases for boost assert utils)
+ */
+
+#include <boost/assert.hpp>
+
+#define ASSERT(...) BOOST_ASSERT(__VA_ARGS__)
+#define ASSERT_MSG(...) BOOST_ASSERT_MSG(__VA_ARGS__)
+#define VERIFY(...) BOOST_VERIFY(__VA_ARGS__)
