d44b176e1c000b73869b174ea244c0c9dc63f928
Azat Khuzhin <a3at.mail@gmail.com>
2013-06-02 11:33:11 +0400

    [util] add stringify macroses

---
+++ src/util/stringify.h
@@ -0,0 +1,19 @@
+
+/**
+ * This file is part of the boostcache package.
+ *
+ * (c) Azat Khuzhin <a3at.mail@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Macros value as C string
+ */
+#define STRVAL(type) STRNAME(type)
+
+/**
+ * Macros name as C string
+ */
+#define STRNAME(type) (#type)
