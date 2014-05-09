ae4e7671b879e110b91d4cbaedbce5dcd51a31e1
Azat Khuzhin <a3at.mail@gmail.com>
2014-05-09 14:24:35 +0400

    hashtable: implement foreach callback using v8 vm

--- src/db/hashtable.cpp
+++ src/db/hashtable.cpp
@@ -11,6 +11,8 @@
 
 #include "hashtable.h"
 
+#include <v8.h>
+
 
 namespace Db
 {
@@ -54,6 +56,39 @@ namespace Db
         }
 
         m_table.erase(value);
+        return CommandHandler::REPLY_TRUE;
+    }
+
+    std::string HashTable::foreach(const CommandHandler::Arguments &arguments)
+    {
+        /**
+         * Common TODO's for using js vm:
+         * - move executor into separate object
+         * - more error handling
+         * - adopt to the newer version of v8 (need Isolate* in constructor)
+         */
+        v8::Isolate* isolate = v8::Isolate::GetCurrent();
+        v8::Locker locker(isolate);
+
+        v8::HandleScope scope;
+        v8::Handle<v8::Context> context = v8::Context::New();
+        v8::Context::Scope enterScope(context);
+        v8::Handle<v8::String> source = v8::String::New(arguments[1].c_str());
+        v8::Handle<v8::Script> script = v8::Script::Compile(source);
+        v8::Handle<v8::Value> sourceResult = script->Run();
+        v8::Handle<v8::Function> function = v8::Handle<v8::Function>::Cast(sourceResult);
+
+        for (std::pair<const Key, Value> &i : m_table) {
+            std::string key(i.first);
+            std::string &value = i.second;
+
+            v8::Local<v8::Value> args[] = {
+                v8::String::New(key.c_str()),
+                v8::String::New(value.c_str())
+            };
+            function->Call(context->Global(), 2, args);
+        }
+
         return CommandHandler::REPLY_TRUE;
     }
 }
